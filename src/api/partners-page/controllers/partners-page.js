03d05c68e3dfb9aec75d0a99af21012fa1845479
Your Name <>
2023-05-31 09:29:09 +0200

    change partners

--- src/api/partners-page/controllers/partners-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * partners-page controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::partners-page.partners-page');
--- src/api/partners-page/services/partners-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * partners-page service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::partners-page.partners-page');

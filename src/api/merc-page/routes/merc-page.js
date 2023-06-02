1a63aa668cd44550e740b1e3956b92587832ca58
Your Name <>
2023-06-02 23:02:35 +0200

    create merch pages

---
+++ src/api/merc-page/routes/merc-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * merc-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::merc-page.merc-page');
---
+++ src/api/merc-page/services/merc-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * merc-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::merc-page.merc-page');

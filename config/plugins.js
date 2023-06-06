244e0ef76b0093fe3507ed7288d0d5e4f8d2bec3
Your Name <>
2023-06-06 21:39:59 +0200

    setting uploud plugin

--- config/plugins.js
+++ config/plugins.js
@@ -6,5 +6,15 @@ module.exports = {
       depthLimit: 100,
       maxLimit: 100,
     }
-  }
+  },
+  upload: {
+    config: {
+      providerOptions: {
+        localServer: {
+          maxage: 300000
+        },
+      },
+      sizeLimit: 5 * 1024 * 1024 // 256mb in bytes
+    },
+  },
 }

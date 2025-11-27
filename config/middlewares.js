08ff58f97362f41681bf11d25cba637e26279cd1
Dmytro Pechunka <pechunka11@gmail.com>
2025-11-27 15:53:46 +0000

    Update Strapi configuration and schema files - 2025-11-27

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,11 +1,31 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://market-assets.strapi.io'],
+          'media-src': ["'self'", 'data:', 'blob:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://burger.hardart.cz', 'https://burger-strapi-demo.hardart.cz'],
+      credentials: true,
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
-  "strapi::body",
+  'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,23 @@
 module.exports = {
-  apps : [{
-    name   : "Burger strapi",
-    script : "yarn start",
-    env_production: {}
-  }],
-
-  deploy : {
-    production : {
-      user : 'dimi',
-      host : ['89.221.216.23'],
-      ref  : 'origin/main',
-      repo : 'git@github.com:simon1400/burger-strapi.git',
-      path : '/var/www/burger/strapi',
-      'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production',
-    }
-  }
+  apps: [{
+    name: 'burger-strapi',
+    cwd: '/opt/burger/strapi',
+    script: 'node_modules/@strapi/strapi/bin/strapi.js',
+    args: 'start',
+    exec_mode: 'fork',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '400M',
+    env: {
+      NODE_ENV: 'production',
+      HOST: '0.0.0.0',
+      PORT: 1333
+    },
+    error_file: '/var/log/pm2/burger-strapi-error.log',
+    out_file: '/var/log/pm2/burger-strapi-out.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    merge_logs: true,
+    time: true
+  }]
 };
